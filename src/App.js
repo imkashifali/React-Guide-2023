91d22110513ae378321b4ddb47a71e395b1b5a16
imkashifali <shafatkashif@gmail.com>
2023-04-18 15:19:53 +0500

    add ref

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React, { useState } from "react";
 
 function App() {
-  const [isValid, setIsValid] = useState(true);
-
-  const changeColors = (event) => {
-    console.log("click me");
-    event.preventDefault();
-
-    setIsValid(false);
-    return;
-  };
   return (
     <div>
-      <p
-        style={{
-          color: !isValid ? "green" : "red",
-          backgroundColor: !isValid ? "yellow" : "green",
-        }}
-      >
-        Goals
-      </p>
-      <button onClick={changeColors}>click me</button>
+      <h1>Ref/Portal</h1>
     </div>
   );
 }
