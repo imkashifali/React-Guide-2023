e70c8386e3c8517aa102440e9aa23335666d4de3
imkashifali <shafatkashif@gmail.com>
2023-04-19 11:14:38 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import AddUser from "./components/Users/AddUser.jsx";
+import UserList from "./components/Users/UserList.jsx";
 
 function App() {
+  const [userList, setUserList] = useState([]);
+
+  const AddUserHandlerData = (enterUserName, enterAge) => {
+    setUserList((prevUserList) => {
+      return [
+        ...prevUserList,
+        { name: enterUserName, age: enterAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <div>
-      <p>Goals Tools</p>
-      <button>click me</button>
+      <AddUser onAddUser={AddUserHandlerData} />
+      <UserList users={userList} />
     </div>
   );
 }
